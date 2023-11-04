ea899f247777ae213d43b17a132d9fc2c545ea4e
hazemelfeky <hazemelfeky25@gmail.com>
2023-11-04 20:34:17 +0200

    feat: add computer choice

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const hands = document.getElementsByClassName("hand");
 const resContainer = document.getElementById("hande-result");
 
+const choices = [
+  { name: "rock", src: "./images/icon-rock.svg" },
+  { name: "scissors", src: "./images/icon-scissors.svg" },
+  { name: "paper", src: "./images/icon-paper.svg" },
+];
+
+function showComputerChoice() {
+  const res = Math.floor(Math.random() * 3);
+  setTimeout(() => {
+    const computerChoice = choices[res];
+    const resImg = document.createElement("img");
+    resImg.src = computerChoice.src;
+    resContainer.append(resImg);
+    resContainer.classList.remove("hide");
+  }, 1000);
+}
+
 function onSelectHand(selectedHand) {
   if (selectedHand.classList.contains("hide")) return;
   for (const hand of hands) {
@@ -10,6 +27,7 @@ function onSelectHand(selectedHand) {
       hand.classList.add("hide");
     }
   }
+  showComputerChoice();
 }
 
 for (const hand of hands) {
